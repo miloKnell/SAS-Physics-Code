b0bfb8dd8561c41d1809f6938a532b9c77fb2a38
Milo Knell <miloknell@gmail.com>
2020-03-17 14:09:43 -0700

    Update hohmann_transfer.py

--- hohmann_transfer.py
+++ hohmann_transfer.py
@@ -4,7 +4,7 @@ r_earth=1.5*10**(11) #distance of Earth from the sun
 r_jupiter = 5.2*1.5*10**(11) #distance of Jupiter from the sun
 e=0.25 #eccentricity of pluto
 a=39.45*1.5*10**(11) #semi major axis of pluto
-
+c_ma = 319.4 #current mean anomoly of pluto
 
 
 def circular_circular(rp,ra):
@@ -35,14 +35,17 @@ def get_period(a):
     '''Given a semi major axis in meters, it returns the orbital period in days using Kepler's 3rd law'''
     return ((a/(1.5*10**(11)))**3)**(1/2)*365
 
-def optimal_launch_deg(r_orgin,e,a):
+def optimal_launch_deg(r_orgin,e,a,ma):
     '''Given the same parameters as circular_eliptical, it calculates the optimal launch window in terms of degree difference between the orgin and target'''
     a_o,a_h = circular_eliptical(r_orgin,e,a,'a_o,a_h')
     p_h = get_period(a_h)/2 #half period of hohmann
     p_o = get_period(a_o) #period of destination
-    window = 180 - (360/p_o)*p_h #degrees ahead of orgin for optimal launch window
-    return window
+    deg_moved_transfer = (360/p_o)*p_h
+    when_pluto_at_180 = (ma-180)/(360/p_o)
+    when_leave = when_pluto_at_180 - p_h #days until launch
+    deg_leave = 180 - deg_moved_transfer #mean anomoly of pluto at launch
+    return when_leave, deg_leave
 
 dv1,dv2 = circular_circular(r_earth,r_jupiter) #Earth to Jupiter
 dv1,dv2 = circular_eliptical(r_earth,e,a) #Earth to Pluto
-launch = optimal_launch_deg(r_earth,e,a) #degrees ahead of Earth for optimal launch window to Pluto
+days_until_launch,mean_anomoly_launch = optimal_launch_deg(r_earth,e,a,c_ma)
